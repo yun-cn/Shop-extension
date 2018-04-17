4c2c6a199d29da2a22e0d38c97b47cb326ffcf23
yunling zhang <yun313350095@gmail.com>
2018-04-17 19:26:42 +0900

    first Commit

---
+++ dub_j_supreme_v2.js
@@ -0,0 +1,59 @@
+// Photo Name of item
+var target = 'CVsY6ESWH2w'
+
+// Your size
+var preferredSize = "Medium"; // "Small", "Medium", "Large", "XLarge"
+
+$('#container article div img').each(function() {
+    var href = $(this).attr('src');
+    //console.log(href.match(target));
+    if (href.match(target)) {
+      $(this)[0].click();
+    }
+});
+
+/*
+automatically choose correct size, if applicable
+*/
+(function waitTillArticlePageIsOpen() {
+  // check if article page has loaded by looking at main image
+  if ($("#img-main")[0]) {
+    // choose appropriate size, if applicable
+    if ($("select")[0]) {
+      for (var i = 0; i < $("select")[0].options.length; i++) {
+        if ($("select")[0].options[i].text == preferredSize) {
+          $("select")[0].selectedIndex = i;
+          break;
+        }
+      }
+    }
+    console.log("done choosing size.")
+    addToCart()
+  } else 
+    setTimeout(function(){ waitTillArticlePageIsOpen(); }, 10);
+    console.log("waiting to load...");
+  
+  return;
+})();
+
+function addToCart() {
+  /*
+  Script to use on item screen
+  */
+  // add to cart
+  document.getElementsByName('commit')[0].click();
+
+  // Wait until cart updates, then go to checkout
+  var itemsCountElm = $("#items-count");
+
+  (function waitTillCartUpdates() {
+    if (itemsCountElm.text() == '') {
+      setTimeout(function(){ waitTillCartUpdates(); }, 10);
+      return;
+    } else {
+      // Click checkout button
+      $(".checkout")[0].click();
+      return;
+    }
+  })();
+}
